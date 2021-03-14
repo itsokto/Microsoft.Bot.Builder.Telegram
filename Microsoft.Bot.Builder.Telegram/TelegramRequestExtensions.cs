0b15deb3818d9a2c0dbb99e819c5fb86c60ba666
itsokto <36706257+itsokto@users.noreply.github.com>
2021-03-15 00:54:55 +0300

    add extension method for create reply from request

--- Microsoft.Bot.Builder.Telegram/TelegramRequestExtensions.cs
+++ Microsoft.Bot.Builder.Telegram/TelegramRequestExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Telegram.Bot.Requests.Abstractions;
+﻿using Microsoft.Bot.Schema;
+using Telegram.Bot.Requests.Abstractions;
 
 namespace Microsoft.Bot.Builder.Telegram
 {
@@ -11,6 +12,16 @@ namespace Microsoft.Bot.Builder.Telegram
 				Method = requestBase.MethodName,
 				Parameters = requestBase
 			};
+		}
+
+		public static Activity CreateBotReply<T>(this ITurnContext turnContext, IRequest<T> requestBase)
+		{
+			var channelData = requestBase.ToChannelData();
+
+			var reply = turnContext.Activity.CreateReply();
+			reply.ChannelData = channelData;
+
+			return reply;
 		}
 	}
 }
